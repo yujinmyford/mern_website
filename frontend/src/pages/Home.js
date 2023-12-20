a898e4e2c2d2db9f1d4db3ba1befb23d824f9d08
Yujin Kim <yujinmyford@gmail.com>
2023-12-20 17:27:58 +0900

    Added functionality for adding new workout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 // components
 import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
 
@@ -27,6 +28,7 @@ const Home = () => {
                      <WorkoutDetails key={workout._id} workout={workout} />
                 ))}
             </div>
+            <WorkoutForm />
         </div>
     )
 }
