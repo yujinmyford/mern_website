49435f5423ada79f78f0e51bea55b4fb983f5d66
Yujin Kim <yujinmyford@gmail.com>
2023-12-20 18:00:03 +0900

    Added React Context

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,5 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext"
 
 // components
 import WorkoutDetails from "../components/WorkoutDetails"
@@ -6,7 +7,7 @@ import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
 
-    const [workouts, setWorkouts] = useState(null)
+    const {workouts, dispatch} = useWorkoutsContext()
 
     useEffect(() => {
         const fetchWorkouts = async () => {
@@ -14,12 +15,12 @@ const Home = () => {
             const json = await response.json()
 
             if (response.ok) {
-                setWorkouts(json)
+                dispatch({type: 'SET_WORKOUTS', payload: json})
             }
         }
 
         fetchWorkouts()
-    }, [])
+    }, [dispatch])
 
     return (
         <div className="home">
